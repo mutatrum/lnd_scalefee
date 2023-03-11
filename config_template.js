aed871cd8213f08d1543a7673251d81c4233b293
mutatrum <mutatrum@gmail.com>
2023-03-11 16:55:05 +0100

    Adjust template

--- config_template.js
+++ config_template.js
@@ -2,8 +2,8 @@ module.exports = {
   lnd: {
     host: 'localhost',
     port: 8080,
-    macaroon: '~/.lnd/data/chain/bitcoin/mainnet/admin.macaroon',
-    certificate: '~/.lnd/tls.cert',
+    macaroon: '/home/lnd/.lnd/data/chain/bitcoin/mainnet/admin.macaroon',
+    certificate: '/home/lnd/.lnd/tls.cert',
     min_fee: 0,
     max_fee: 150,
     threshold: 10,
