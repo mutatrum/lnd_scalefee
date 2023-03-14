f36e80fbd6c3cbc270cc5c93fbe9454a2cecd502
mutatrum <mutatrum@gmail.com>
2023-03-14 16:26:04 +0100

    Strip trailing whitespace

--- scalefee.js
+++ scalefee.js
@@ -33,27 +33,27 @@ async function onSchedule(arg) {
   try {
     const fees = await request('GET', '/v1/fees')
     const channels = await request('GET', '/v1/channels?active_only=true')
-  
+
     for (var channel of channels.channels) {
-      
+
       let fee = fees.channel_fees.find(fee => fee.chan_id === channel.chan_id)
-      
+
       if (!fee) return
 
       const new_fee = calculateNewFee(channel)
-      
+
       if (Math.abs(fee.fee_per_mil - new_fee) > config.lnd.threshold) {
         const nodeinfo = await request('GET', `/v1/graph/node/${channel.remote_pubkey}`)
 
         console.log(`${nodeinfo.node.alias}: ${fee.fee_per_mil} -> ${new_fee} ppm`)
 
         if (arg) continue
-        
+
         const [funding_txid_str, output_index] = channel.channel_point.split(':')
 
         const edgeinfo = await request('GET', `/v1/graph/edge/${channel.chan_id}`)
         const policy = edgeinfo.node1_pub === identity_pubkey ? edgeinfo.node1_policy : edgeinfo.node2_policy
-  
+
         let requestBody = {
           chan_point: {
             funding_txid_str: funding_txid_str,
@@ -64,7 +64,7 @@ async function onSchedule(arg) {
           time_lock_delta: policy.time_lock_delta
         }
         let result = await request('POST', '/v1/chanpolicy', requestBody)
-  
+
         if (result.failed_updates.length > 0) {
           console.log(result)
         }
@@ -85,15 +85,15 @@ function calculateNewFee(channel) {
 }
 
 function request(method, uri, requestBody) {
-  const options = { 
+  const options = {
     host: config.lnd.host,
     port: config.lnd.port,
     path: uri,
-    method: method, 
+    method: method,
     headers : {
       'Accept': 'application/json',
       'Grpc-Metadata-macaroon': macaroon
-    }, 
+    },
     ca: certificate
   }
   return new Promise(function(resolve, reject) {
